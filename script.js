123c1174f64be26080ec1445049806d73677d8c2
tilipinpin <79435607+tilipinpin@users.noreply.github.com>
2024-10-23 10:39:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -184,8 +184,8 @@ document.addEventListener("DOMContentLoaded", function() {
             weeksContainer.appendChild(weekDiv);
         }
 
-        updateMonthLabels(startDate, isYearSelected ? selectedYear : "this year");
-        updateStats(yearTotalDays, yearTotalHours, daysOver16Hours, daysOver17Hours, overtimeDays, isYearSelected ? selectedYear :"this year");
+        updateMonthLabels(startDate, isYearSelected ? selectedYear : new Date().getFullYear());
+        updateStats(yearTotalDays, yearTotalHours, daysOver16Hours, daysOver17Hours, overtimeDays, isYearSelected ? selectedYear :new Date().getFullYear());
     }
 
     function updateStats(days, hours, daysOver16Hours, daysOver17Hours, overtimeDays, year) {
